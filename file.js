ecb21e8a910d24c14be8f9187d51f8ee0f095863
Manik Gaur <gaur.manik.mg@gmail.com>
2022-10-06 12:57:56 +0530

    m

---
+++ file.js
@@ -0,0 +1,23 @@
+function twoDimensionalArray(a,b){
+    let arr= [];
+    for(let i=0; i<a;i++)
+    {
+        for(let j=0;j<b;j++)
+        {
+            arr[i]= [];
+        }
+    }
+    for(let i=0;i<a;i++)
+    {
+        for(let j =0;j<b;j++)
+        {
+            arr[i][j]=j;
+        }
+
+    }
+    return arr;
+}
+const x=2;
+const y=3;
+const result = twoDimensionalArray(x,y);
+document.write(result);
